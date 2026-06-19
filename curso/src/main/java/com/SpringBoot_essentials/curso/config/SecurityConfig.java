715178709f016ccf7e01fbc048c35da758017e33
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2026-06-18 23:31:57 -0300

    Adiciona proteção de URLs por padrão com requestMatchers no SecurityConfig

--- curso/src/main/java/com/SpringBoot_essentials/curso/config/SecurityConfig.java
+++ curso/src/main/java/com/SpringBoot_essentials/curso/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.SpringBoot_essentials.curso.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,14 +22,43 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/animes/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/animes").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/animes").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/animes/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
-    public PasswordEncoder passwordEnconder (){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("jeferson")
+                .password("{noop}academy")
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails comum = User.builder()
+                .username("comum")
+                .password("{noop}comum")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, comum);
     }
 }
